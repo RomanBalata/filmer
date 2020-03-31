23dbb6a6e091bdf5e019b40e98d4d54a56edaa08
Roman.Balata <roman.balata@ekreative.com>
2020-03-31 12:09:35 +0300

    init project with CRA, setup eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+    parser: "babel-eslint",
+    env: {
+      es6: true,
+      browser: true,
+    },
+    parserOptions: {
+      ecmaFeatures: {
+        jsx: true
+      },
+      ecmaVersion: 2018,
+      sourceType: "module"
+    },
+    extends: ["airbnb", "prettier", "prettier/react"],
+    plugins: ["prettier"],
+    rules: {
+      "prettier/prettier": "error",
+      "import/prefer-default-export": "off",
+      "no-use-before-define": "off",
+      "react/jsx-filename-extension": ["error", { extensions: [".js", ".jsx"] }],
+      "react/prop-types": "off",
+      "no-shadow": "off"
+    },
+    settings: {
+      "import/resolver": {
+        node: {
+          paths: ["src"]
+        }
+      }
+    }
+  };
---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function App() {
+  return <div>Filmer</div>;
+}
+
+export default App;
